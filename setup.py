1617ec36fdd7595560819e5ef586b924d4d3c593
shirobu2400 <shirobu_2400kskm@yahoo.co.jp>
2020-04-15 16:40:37 +0900

    update directory

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 setup(
     name="mmdpy",
+    packages=["mmdpy"],
     version="0.0.1",
+    license="MIT",
+    author='shirobu2400',
+    author_email='shirobuwq2400kskm@gmail.com',
+    url='https://github.com/shirobu2400/mmdpy',
+    description="mmd viewer @ python",
     install_requires=[  "PyOpenGL",
                         "PyOpenGL_accelerate",
                         "numpy",
-                        "Pillow"],
-    extras_require={
-        "develop": [   "PyOpenGL",
-                        "PyOpenGL_accelerate",
-                        "numpy",
                         "Pillow"]
-    }
 )
